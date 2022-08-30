9e7f68e1d2c8f1fb39c55e07dce81d057cbc0066
Aadhithya <70188466+Mr-Aadhithya@users.noreply.github.com>
2022-08-30 17:48:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import json
+from wsgiref import headers
+import requests
+
+headers ={
+    'X-CMC_PRO_API_KEY' : 'd6f394d4-3dc2-4d3d-9293-25f39c5693be',
+    'Accepts' : 'application/json'
+
+}
+
+params={
+    'start' :  '1',
+    'limit' :  '5',
+    'convert' : 'INR'
+
+}
+url='https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+
+json=requests.get(url,params=params,headers=headers).json()
+
+#print(json)
+
+coins =json['data']
+
+print("All prices in ruprees")
+
+for x in coins:
+    
+    print(x['symbol'],x['quote']['INR']['price'])
+
